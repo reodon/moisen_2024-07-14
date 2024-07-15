5c2db0e286ccff106111f2d13e3e8e7dd132076e
reodon <reodon@users.noreply.github.com>
2024-07-15 13:05:33 +0900

    wip fix: Web Speech API: SpeechRecognition

--- src/app/_/hooks/useSpeechRecognition.js
+++ src/app/_/hooks/useSpeechRecognition.js
@@ -1,39 +1,92 @@
+import { useState, useEffect } from "react";
+
 const useSpeechRecognition = () => {
   // if (!window.SpeechRecognition) {
   //   return {};
   // }
+  const [isRecording, setIsRecording] = useState(false);
+  // const [text, setText] = useState<string>("");
+  // const [transcript, setTranscript] = useState<string>("");
+  // const [recognition, setRecognition] = useState<SpeechRecognition | null>(
+  //   null
+  // );
+  const [text, setText] = useState("");
+  const [transcript, setTranscript] = useState("");
+  const [recognition, setRecognition] = useState(null);
 
-  const SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
-  const rec = new SpeechRecognition();
-  rec.continuous = true;
-  rec.interimResults = false;
-  rec.lang = "ja-JP" // "en-US"なら英語で認識してくれる。指定がないとブラウザの言語になる
-
-  rec.onresult = function (e) {
-    console.log("on result")
-
-    for (var i = e.resultIndex; i < e.results.length; ++i) {
-      if (e.results[i].isFinal) {
-        const str = e.results[i][0].transcript;
-        console.log('Your speaking: ' + str);
-      }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const recognition = new webkitSpeechRecognition();
+      recognition.lang = "ja-JP";
+      recognition.continuous = true;
+      recognition.interimResults = true;
+      setRecognition(recognition);
     }
-  }
+  }, []);
 
-  const start = () => {
-    console.log("SpeechRecognition: start");
-    rec.start();
-  };
+  useEffect(() => {
+    if (!recognition) return;
+    if (isRecording) {
+      recognition.start();
+    } else {
+      recognition.stop();
+      setText("");
+    }
+  }, [isRecording]);
 
-  const stop = () => {
-    console.log("SpeechRecognition: stop");
-    rec.stop();
-  };
+  useEffect(() => {
+    if (!recognition) return;
+    recognition.onresult = (event) => {
+      const results = event.results;
+      for (let i = event.resultIndex; i < results.length; i++) {
+        if (results[i].isFinal) {
+          setText((prevText) => prevText + results[i][0].transcript);
+          setTranscript("");
+        } else {
+          setTranscript(results[i][0].transcript);
+        }
+      }
+    };
+  }, [recognition]);
 
   return {
-    start,
-    stop,
+    text,
+    transcript,
+    isRecording,
+    setIsRecording,
   };
+
+  // const SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+  // const rec = new SpeechRecognition();
+  // rec.continuous = true;
+  // rec.interimResults = false;
+  // rec.lang = "ja-JP" // "en-US"なら英語で認識してくれる。指定がないとブラウザの言語になる
+
+  // rec.onresult = function (e) {
+  //   console.log("on result")
+
+  //   for (var i = e.resultIndex; i < e.results.length; ++i) {
+  //     if (e.results[i].isFinal) {
+  //       const str = e.results[i][0].transcript;
+  //       console.log('Your speaking: ' + str);
+  //     }
+  //   }
+  // }
+
+  // const start = () => {
+  //   console.log("SpeechRecognition: start");
+  //   rec.start();
+  // };
+
+  // const stop = () => {
+  //   console.log("SpeechRecognition: stop");
+  //   rec.stop();
+  // };
+
+  // return {
+  //   start,
+  //   stop,
+  // };
 };
 
 export {
--- src/app/samples/speech-rec/SpeechRecPage.jsx
+++ src/app/samples/speech-rec/SpeechRecPage.jsx
@@ -4,14 +4,19 @@ import { useSpeechRecognition } from "@/app/_/hooks/useSpeechRecognition";
 
 export default function SpeechRecPage() {
   const {
-    start: startSpeechRec,
-    stop: stopSpeechRec,
+    text,
+    transcript,
+    isRecording,
+    setIsRecording,
   } = useSpeechRecognition();
 
   return (<>
     <main className="flex min-h-screen flex-col gap-4 p-24">
-      <button className="btn" onClick={() => startSpeechRec()}>Start</button>
-      <button className="btn" onClick={() => stopSpeechRec()}>Stop</button>
+      <button className="btn" onClick={() => setIsRecording(prev => !prev)}>{isRecording ? 'Stop' : 'Start'}</button>
+      <div>
+        <p>transcript: {transcript}</p>
+        <p>text: {text}</p>
+      </div>
     </main>
   </>);
 }
